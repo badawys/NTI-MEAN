3d61afdec9666e1ec72e92199bfde625ce8d0997
Ahmad Elbadawy <ahmad.elbadawy@outlook.com>
2026-08-05 20:27:08 +0300

    fix: remove unnecessary type casting for requestLogger in course routes

--- Day 6/express-js-session-demo/src/routes/courses.routes.ts
+++ Day 6/express-js-session-demo/src/routes/courses.routes.ts
@@ -1,4 +1,4 @@
-import { Router, RequestHandler } from "express";
+import { Router } from "express";
 import { getAllCourses, createCourse } from "../controllers/courses.controller";
 import { requestLogger } from "../middleware/requestLogger";
 import { validateCreateCourse } from "../middleware/validateCreateCourse";
@@ -7,12 +7,12 @@ import { getCourseById } from "../controllers/courses.controller";
 const router = Router();
 
 // Get all courses
-router.get("/", requestLogger as unknown as RequestHandler, getAllCourses);
+router.get("/", requestLogger, getAllCourses);
 
 // Create a new course
 router.post("/", requestLogger, validateCreateCourse, createCourse);
 
 // Get a course by ID
-router.get("/:id", requestLogger as unknown as RequestHandler, getCourseById);
+router.get("/:id", requestLogger, getCourseById);
 
 export default router;
